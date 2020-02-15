acb032c7fa5cafd6f21ed231b350bc84f4a48083
Jace-Mix <jace.mixon@knights.ucf.edu>
2020-02-15 18:54:50 -0500

    Added new DeleteContact API endpoint

---
+++ contact_manager/src/backend/API/DeleteContactFull.php
@@ -0,0 +1,47 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$conn = new mysqli("localhost", "group5", "JavascriptPhpMysql", "contact_manager");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$sql = "DELETE FROM contacts WHERE userID='". $inData["userID"] . "' and firstName='" . $inData["firstName"] . "' and lastName='" . $inData["lastName"] . "' and email='" . $inData["email"] . "' and phoneNumber='" . $inData["phoneNumber"] . "'";
+		$result = $conn->query($sql);
+		if ($result == false)
+		{
+			$conn->close();
+			returnWithError( "Error in Deleting Contacts" );
+		}
+		
+		$conn->close();
+		returnWithSuccess();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+	
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithSuccess()
+	{
+		$retValue = '{"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
